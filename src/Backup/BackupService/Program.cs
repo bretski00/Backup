8ec4afcc8d024af0b320972c97a7f512c93ac448
bretski00 <bretski00@gmail.com>
2015-06-09 21:50:20 -0500

    Add top shelf actions

--- src/Backup/BackupService/Program.cs
+++ src/Backup/BackupService/Program.cs
@@ -8,8 +8,22 @@ namespace Backup
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            HostFactory.Run(x =>                                 //1
+            {
+                x.Service<TownCrier>(s =>                        //2
+                {
+                    s.ConstructUsing(name => new TownCrier());     //3
+                    s.WhenStarted(tc => tc.Start());              //4
+                    s.WhenStopped(tc => tc.Stop());               //5
+                });
+                x.RunAsLocalSystem();                            //6
+
+                x.SetDescription("Blazing Lite Simple Backup for nightly storage");        //7
+                x.SetDisplayName("BlazingLiteBackup");                       //8
+                x.SetServiceName("BlazingLiteBackup");                       //9
+            });   
         }
     }
 }
